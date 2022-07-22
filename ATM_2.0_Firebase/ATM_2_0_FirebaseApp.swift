5d02bfefd2dfd2dfc67049e5e30463eed68afca8
AppleDev <Shinyatheris@gmail.com>
2022-07-22 14:24:22 +0200

    initial commit

---
+++ ATM_2.0_Firebase/ATM_2_0_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATM_2_0_FirebaseApp.swift
+//  ATM_2.0_Firebase
+//
+//  Created by user217621 on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ATM_2_0_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
