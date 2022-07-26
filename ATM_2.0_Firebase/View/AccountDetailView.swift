f184147ba841cf5c9360662a5ce22f7f4452aab5
AppleDev <Shinyatheris@gmail.com>
2022-07-26 16:52:20 +0200

    [ATM2.0Firebase][Add] Two placeholder views

---
+++ ATM_2.0_Firebase/View/AccountDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  AccountDetailView.swift
+//  ATM_2.0_Firebase
+//
+//  Created by user217621 on 7/25/22.
+//
+
+import SwiftUI
+
+struct AccountDetailView: View {
+    var body: some View {
+        Text("Account Detail View")
+    }
+}
+
+struct AccountDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountDetailView()
+    }
+}
---
+++ ATM_2.0_Firebase/View/RegisterView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterView.swift
+//  ATM_2.0_Firebase
+//
+//  Created by user217621 on 7/25/22.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text("Register View")
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
