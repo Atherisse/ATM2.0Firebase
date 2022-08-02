afb894a701d9b5e494603a04f5f1b3ec1d67a1ed
AppleDev <Shinyatheris@gmail.com>
2022-08-02 13:36:19 +0200

    [ATM2.0Firebase][Add] LogInViewModel containing method for login authentication

---
+++ ATM_2.0_Firebase/ViewModel/LogInViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  LogInViewModel.swift
+//  ATM_2.0_Firebase
+//
+//  Created by user217621 on 7/29/22.
+//
+
+import Foundation
+
+struct LogInViewModel {
+    
+    //hard coded test values
+    var testAccountNumber: String = "9875-4852-3698-2525"
+    var testPassword: String = "Ilikefish1971"
+    var testPinNumber: String = "1971"
+    
+    func authenticatingLogInDetails(accountNumber: String, password: String, pinNumber: String) -> Bool {
+        //determine login validity (for now it compares with hard coded values)
+        let logInIsValid: Bool = accountNumber == testAccountNumber && password == testPassword && pinNumber == testPinNumber
+        
+        //trigger logic
+        if logInIsValid {
+            return true //trigger NavigationLink
+        }
+        
+        return false
+    }
+    
+    func determineShowAlert(logInValidity: Bool) -> Bool {
+        
+        //if the login details are invalid, show alert
+        if logInValidity {
+            return false
+        }
+        return true
+    }
+}
